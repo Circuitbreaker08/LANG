612905954d330a5ecd15642f1fb7595300650797
BoomBoomMushroom <77977795+BoomBoomMushroom@users.noreply.github.com>
2025-03-02 02:32:22 -0500

    primes generator fix a bit

--- primes.py
+++ primes.py
@@ -3,6 +3,10 @@ import json
 with open("prime_table.json") as f: prime_table = json.loads(f.read())
 
 def prime():
+    gen = prime_generator()
+    return next(gen)
+
+def prime_generator():
     while True:
         for x in prime_table:
             yield x
